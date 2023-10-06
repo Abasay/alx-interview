0355cb453f8a61535629aae408206fd637ef7399
Abasay <abdulsalamasheem@gmail.com>
2023-10-06 01:15:32 +0000

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,16 +8,17 @@ def canUnlockAll(boxes):
     Function to check if boxes can be unlocked
     """
 
-    keys = boxes[0]
-    for i in range(0, len(boxes)):
-        for key in keys:
-            for i in boxes[key]:
-                if not (i in keys):
-                    keys.append(i)
 
-    for i in range(0, len(boxes)):
-        if i == 0:
-            continue
-        if not (i in keys):
-            return False
-    return True
+    unlocked = False
+    keys = boxes[0]
+
+    for key in keys:
+        for i in boxes[key]:
+            if not (i in keys):
+                keys.append(i)
+        for i in range(1, len(boxes)):
+            if i in keys:
+                unlocked = True
+            else:
+                unlocked = False
+    return unlocked
