59a2d06cb82e4ec1825d5725e70909d0bd6f523d
Abasay <abdulsalamasheem@gmail.com>
2023-10-06 00:57:40 +0000

    locbpxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,14 @@
 def canUnlockAll(boxes):
+    unlocked = False
     keys = boxes[0]
 
-    for i in range(0, len(boxes)):
-        for key in keys:
-            for i in boxes[key]:
-                if not (i in keys):
-                    keys.append(i)
-
-    for i in range(0, len(boxes)):
-        if i == 0:
-            continue
-        if not (i in keys):
-            return False
-
-    return True
+    for key in keys:
+        for i in boxes[key]:
+            if not (i in keys):
+                keys.append(i)
+        for i in range(1, len(boxes)):
+            if i in keys:
+                unlocked = True
+            else:
+                unlocked = False
+    return unlocked
