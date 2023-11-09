a1b66213808f32a162f75572992221babfc2183b
Abasay <abdulsalamasheem@gmail.com>
2023-11-09 14:44:38 -0800

    starwars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+const process = require('process');
+const args = process.argv;
+const url = `https://swapi-api.alx-tools.com/api/films/${args[2]}`
+
+const options = {
+    url: url,
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Accept-Charset': 'utf-8',
+        'User-Agent': 'alx-student'
+    }
+};
+
+request(options, (err, resp, body) => {
+    if (err) console.log(err);
+    let res = JSON.parse(body);
+    let characters = res.characters
+
+    for (let elem = 0; elem < characters.length; elem++) {
+	request(characters[elem], (err, resp, body) => {
+	    let characterRes = JSON.parse(body);
+	    console.log(characterRes.name);
+	});
+    };
+});
