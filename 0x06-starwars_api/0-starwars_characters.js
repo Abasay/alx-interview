692b71626f0d227e39369073bd49f28e4982e744
Abasay <abdulsalamasheem@gmail.com>
2023-11-09 16:01:22 -0800

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,32 +3,30 @@
 const request = require('request');
 const process = require('process');
 const args = process.argv;
-const url = `https://swapi-api.alx-tools.com/api/films/${args[2]}`
+const url = `https://swapi-api.alx-tools.com/api/films/${args[2]}`;
 
 const options = {
-    url: url,
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json',
-        'Accept-Charset': 'utf-8',
-        'User-Agent': 'alx-student'
-    }
+  url,
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    'Accept-Charset': 'utf-8',
+    'User-Agent': 'alx-student'
+  }
 };
 
-request(options, async(err, resp, body) => {
-    if (err) return console.error(err);
-    
-    let characters =  JSON.parse(body).characters;
-    //await queue result until they resolve in order
-    for (const characterUrl of characters) {
-	await new Promise((resolve, reject) => {
-	    request(characterUrl, (err, res, body) => {
-		if (err) return console.error(err);
+request(options, async (err, resp, body) => {
+  if (err) return console.error(err);
 
-		console.log(JSON.parse(body).name)
-
-		resolve();
-	    });
-	});
-    }
+  const characters = JSON.parse(body).characters;
+  // await queue result until they resolve in order
+  for (const characterUrl of characters) {
+    await new Promise((resolve, reject) => {
+      request(characterUrl, (err, res, body) => {
+        if (err) return console.error(err);
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
 });
