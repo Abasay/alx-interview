71b9403b8c78a87693d82914049a17d999af92b3
Abasay <abdulsalamasheem@gmail.com>
2023-11-19 17:38:47 +0100

    rotate

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,48 +8,59 @@ def rotate_2d_matrix(matrix):
     """
     Function that rotates a matrix
     """
-    index = 0
+    for x, y in enumerate(zip(*reversed(matrix))):
+        matrix[x] = list(y)
+    # index = 0
 
-    resList = []
-    for elem in range(0, len(matrix)):
-        newList = []
-        for row in matrix:
-            newList.append(row[index])
-        index += 1
-        newList.reverse()
-        resList.append(newList)
-    return resList
+    # resList = []
+    # for elem in range(0, len(matrix)):
+    #     newList = []
+    #     for row in matrix:
+    #         newList.append(row[index])
+    #     index += 1
+    #     newList.reverse()
+    #     matrix[elem] = newList
+    # resList.append(newList)
+
+    # matrix = resList
+    # print(matrix)
 
 
-print(rotate_2d_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+if __name__ == "__main__":
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-print(
-    rotate_2d_matrix(
-        [[1, 11, 13, 19], [41, 32, 6, 22], [55, 21, 41, 66], [77, 29, 78, 2]]
-    )
-)
+    rotate_2d_matrix(matrix)
+    print(matrix)
 
-print(
-    rotate_2d_matrix(
-        [
-            [1, 2, 3, 4, 5],
-            [6, 7, 8, 9, 10],
-            [11, 12, 13, 14, 15],
-            [16, 17, 18, 19, 20],
-            [21, 22, 23, 24, 25],
-        ]
-    )
-)
+# print(rotate_2d_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
 
-print(
-    rotate_2d_matrix(
-        [
-            [5, 8, 11, 14, 17, 20],
-            [23, 26, 29, 32, 35, 38],
-            [41, 44, 47, 50, 53, 56],
-            [59, 62, 65, 68, 71, 74],
-            [77, 80, 83, 86, 89, 92],
-            [95, 98, 101, 104, 107, 110],
-        ]
-    )
-)
+# print(
+#     rotate_2d_matrix(
+#         [[1, 11, 13, 19], [41, 32, 6, 22], [55, 21, 41, 66], [77, 29, 78, 2]]
+#     )
+# )
+
+# print(
+#     rotate_2d_matrix(
+#         [
+#             [1, 2, 3, 4, 5],
+#             [6, 7, 8, 9, 10],
+#             [11, 12, 13, 14, 15],
+#             [16, 17, 18, 19, 20],
+#             [21, 22, 23, 24, 25],
+#         ]
+#     )
+# )
+
+# print(
+#     rotate_2d_matrix(
+#         [
+#             [5, 8, 11, 14, 17, 20],
+#             [23, 26, 29, 32, 35, 38],
+#             [41, 44, 47, 50, 53, 56],
+#             [59, 62, 65, 68, 71, 74],
+#             [77, 80, 83, 86, 89, 92],
+#             [95, 98, 101, 104, 107, 110],
+#         ]
+#     )
+# )
