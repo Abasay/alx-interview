2838bb83d9455003ef53e0d975c8445a7c423f60
Abasay <abdulsalamasheem@gmail.com>
2023-12-10 01:06:20 +0100

    the prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Define isWinner function, a solution to the Prime Game problem
+"""
+
+
+def primes(n):
+    """Return list of prime numbers between 1 and n inclusive
+    Args:
+     n (int): upper boundary of range. lower boundary is always 1
+    """
+    prime = []
+    sieve = [True] * (n + 1)
+    print(sieve)
+    for p in range(2, n + 1):
+        if sieve[p]:
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
+
+
+def isWinner(x, nums):
+    """
+    Determines winner of Prime Game
+    Args:
+        x (int): no. of rounds of game
+        nums (int): upper limit of range for each round
+    Return:
+        Name of winner (Maria or Ben) or None if winner cannot be found
+    """
+    if x is None or nums is None or x == 0 or nums == []:
+        return None
+    maria = ben = 0
+    for i in range(x):
+        prime = primes(nums[i])
+        if len(prime) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if maria > ben:
+        return "Maria"
+    elif ben > maria:
+        return "Ben"
+    return None
